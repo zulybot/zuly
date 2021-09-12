07669c15afe615813b49d4459293d1b7c1b9d6bb
MrAndre <71835146+eiandremoreira@users.noreply.github.com>
2021-09-12 12:17:21 -0300

    Update BotinfoCommand.js

--- src/commands/Info/BotinfoCommand.js
+++ src/commands/Info/BotinfoCommand.js
@@ -27,8 +27,8 @@ module.exports = class BotinfoCommand {
 		embed.title(`🤖 Botinfo | ${global.zuly.user.username}`);
 		embed.thumbnail(global.zuly.user.avatarURL);
 		embed.description(ctx.idioma.botinfo.texto.replace('%bot', global.zuly.user.username).replace('%g', global.zuly.guilds.size).replace('%u', global.zuly.guilds.reduce((acc, guild) => acc + guild.memberCount, 0)));
-		// embed.field(`SO`, `${process.platform}`)
-		// embed.field(`Uso Da Memória`, `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)+'mb'}/${(os.totalmem() / 1024 / 1024).toFixed(2)+'mb'}`)
+		embed.field(`SO`, `${process.platform}`)
+		embed.field(`Uso Da Memória`, `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)+'mb'}/${(os.totalmem() / 1024 / 1024).toFixed(2)+'mb'}`)
 		embed.color('#ffcbdb');
 		ctx.send(embed.create);
 	}
