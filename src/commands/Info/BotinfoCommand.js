2d3e0c66fbb6f0bd2c13954eb796e44a8d9df4e0
yADGithub <2007andredoe@gmail.com>
2021-09-18 10:58:28 -0300

    Update botinfo

--- src/commands/Info/BotinfoCommand.js
+++ src/commands/Info/BotinfoCommand.js
@@ -23,12 +23,13 @@ module.exports = class BotinfoCommand {
 
 	async run (ctx) {
 		// eslint-disable-next-line new-cap
+		const os = require('os');
 		const embed = new ctx.embed();
 		embed.title(`🤖 Botinfo | ${global.zuly.user.username}`);
 		embed.thumbnail(global.zuly.user.avatarURL);
 		embed.description(ctx.idioma.botinfo.texto.replace('%bot', global.zuly.user.username).replace('%g', global.zuly.guilds.size).replace('%u', global.zuly.guilds.reduce((acc, guild) => acc + guild.memberCount, 0)));
-		embed.field(`SO`, `${process.platform}`)
-		embed.field(`Uso Da Memória`, `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)+'mb'}/${(os.totalmem() / 1024 / 1024).toFixed(2)+'mb'}`)
+		embed.field('SO', `${process.platform}`);
+		embed.field('Uso Da Memória', `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2) + 'mb'}/${(os.totalmem() / 1024 / 1024).toFixed(2) + 'mb'}`);
 		embed.color('#ffcbdb');
 		ctx.send(embed.create);
 	}
