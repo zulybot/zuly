6d8f02dda0904f4fc4ec43396f74dfc10a9e704c
ADG <65923257+andrelucaas@users.noreply.github.com>
2021-10-12 15:58:09 -0300

    New translations en-US.js (German)

--- src/Translations/de-DE.js
+++ src/Translations/de-DE.js
@@ -55,7 +55,7 @@ module.exports = {
 		res: 'Resultado'
 	},
 	botinfo: {
-		texto: 'Olá, sou a %bot, um bot com funcionalidades únicas, que todo servidor precisa! Atualmente conheço **%u pessoas** diferentes que utilizam de minhas funcionalidades e estou em **%g servidores** diferentes.\n\nSou um projeto de [And.](https://github.com/andrelucaas) e [Dexy](https://github.com/eiandremoreira) desenvolvida em [Eris](https://abal.moe/Eris/) utilizando [Javascript](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript) & [NodeJS](https://nodejs.org/en/)',
+		texto: 'Hi, I\'m %bot, a bot with unique features that every server needs! I currently know **%u different people** who use my features and I\'m on **%g different servers**.\n\nI\'m developed by `%devs` using [Eris](https://abal.moe/Eris/) using [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript) & [ NodeJS](https://nodejs.org/en/)',
 		recursos: 'Recursos:'
 	},
 	messages: {
