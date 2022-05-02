2902785c9cf49c50b60fab9c96b29726e3dd6cc0
andrelucaas <2007andredoe@gmail.com>
2022-05-02 14:38:14 -0300

    nsei oq é o bug, dps vejo

--- src/Commands/Economy/ProfileCommand.js
+++ src/Commands/Economy/ProfileCommand.js
@@ -56,6 +56,10 @@ module.exports = class EvalCommand {
 		  const { fillTextWithTwemoji } = require('node-canvas-with-twemoji-and-discord-emoji');
 		  const user = ctx.args[0] ? await global.zuly.users.fetch(ctx.args[0]).catch(() => ctx.message.author) : ctx.message.author;
 		  const back = await global.zuly.db.get(`background-${user.id}`) || './assets/images/backgrounds/default.jpg';
+		  const userPremium = await global.zuly.getPremium('doador', user.id);
+		  console.log(userPremium);
+		  const bugHunter = await global.zuly.getBugHunter(user.id);
+		  const devs = await global.zuly.db.get('devs');
 		  registerFont('./assets/fonts/Dunkin.otf', {
 			family: 'Dunkin'
 		  });
@@ -69,39 +73,47 @@ module.exports = class EvalCommand {
 		  foto.drawImage(avatar, 75, 10, 160, 160);
 		  foto.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-		  const userPremium = await global.zuly.getPremium('doador', user.id);
-		  const bugHunter = await global.zuly.getBugHunter(user.id);
+		  let userDev = false;
 
-		  if (bugHunter && userPremium) {
-			const bug = await loadImage('./assets/images/badges/bughunter.png');
-			const early = await loadImage('./assets/images/badges/earlysupport.png');
-			foto.drawImage(early, 650, 60, 60, 50);
-			foto.drawImage(bug, 700, 60, 60, 50);
-		  }
-		else if (bugHunter) {
-			const early = await loadImage('./assets/images/badges/bughunter.png');
-			foto.drawImage(early, 650, 60, 60, 50);
-		  }
-		else if (userPremium) {
-			const early = await loadImage('./assets/images/badges/earlysupport.png');
-			foto.drawImage(early, 650, 60, 60, 50);
-		  }
+		  setTimeout(async () => {
+			if (devs.includes(user.id)) {
+				userDev = true;
+			}
+			if (userDev) {
+				const dev = await loadImage('./assets/images/badges/botdev.png');
+				foto.drawImage(dev, 585, 60, 45, 45);
+			}
+			if (bugHunter && userPremium) {
+				const bug = await loadImage('./assets/images/badges/bughunter.png');
+				const early = await loadImage('./assets/images/badges/earlysupport.png');
+				foto.drawImage(early, 650, 60, 45, 45);
+				foto.drawImage(bug, 700, 60, 45, 45);
+			  }
+			else if (bugHunter) {
+				const early = await loadImage('./assets/images/badges/bughunter.png');
+				foto.drawImage(early, 650, 60, 45, 45);
+			  }
+			else if (userPremium) {
+				const early = await loadImage('./assets/images/badges/earlysupport.png');
+				foto.drawImage(early, 650, 60, 45, 45);
+			  }
 
-		  if (user.username.length > 9) {
-			foto.font = '17px Dunkin';
-		  }
-		  if (user.username.length < 9) {
-			foto.font = '30px Dunkin';
-		  }
-		  foto.fillStyle = '#ffffff';
-		  await fillTextWithTwemoji(foto, `${user.username.toUpperCase()}#${user.discriminator}`, canvas.width / 2.5, canvas.height / 6.5);
-		  foto.font = '17px Dunkin';
-		  await fillTextWithTwemoji(foto, about.match(/.{1,65}/g).join('\n'), canvas.width / 28, canvas.height / 1.17);
-		const { MessageAttachment } = require('discord.js');
-		const attachment = new MessageAttachment(canvas.toBuffer(), 'profile.png');
-		  ctx.message.channel.slashReply({
-			content: ctx.message.author.mention,
-			files: [attachment],
-		  });
+			  if (user.username.length > 9) {
+				foto.font = '17px Dunkin';
+			  }
+			  if (user.username.length < 9) {
+				foto.font = '30px Dunkin';
+			  }
+			  foto.fillStyle = '#ffffff';
+			  await fillTextWithTwemoji(foto, `${user.username.toUpperCase()}#${user.discriminator}`, canvas.width / 2.5, canvas.height / 6.5);
+			  foto.font = '17px Dunkin';
+			  await fillTextWithTwemoji(foto, about.match(/.{1,65}/g).join('\n'), canvas.width / 28, canvas.height / 1.17);
+			const { MessageAttachment } = require('discord.js');
+			const attachment = new MessageAttachment(canvas.toBuffer(), 'profile.png');
+			  ctx.message.channel.slashReply({
+				content: ctx.message.author.mention,
+				files: [attachment],
+			  });
+		  }, 1000);
 	}
 };
