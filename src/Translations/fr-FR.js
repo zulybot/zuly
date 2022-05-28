d16219e6f29a2b4ce70d27fca266dc84d6527cbc
ADG <2007andredoe@gmail.com>
2022-05-28 17:23:33 -0300

    New translations en-US.js (French)

--- src/Translations/fr-FR.js
+++ src/Translations/fr-FR.js
@@ -1,5 +1,6 @@
 module.exports = {
 	lang: 'fr',
+	div: '<:zu_minecraft:980194995485175809> {{user}} **|** Are you looking for a Minecraft server? Come join [ZulyMC](https://discord.gg/mcbu7wsX8W)\n>> ⤷ **`IP:` **mc.zulybot.xyz;\n⤷ **`Versions:`** Java (1.17.1/Original/Pirate).',
 	afk: {
 		set: ':zzz: {{user}} **|** You are afk, reason: `{{status}}',
 		remove: ':zzz: {{user}} **|** You are no longer afk',
@@ -417,7 +418,8 @@ module.exports = {
 		title: 'Avatar depuis:',
 		download: 'Lien de téléchargement :',
 		hex: 'Hex à utiliser dans votre profil :',
-		click: 'Cliquez ici'
+		click: 'Cliquez ici',
+		footer: 'If the plan doesn\'t work, change the plan, not the goal.'
 	},
 	perfil: {
 		ngm: 'NON-BODY',
