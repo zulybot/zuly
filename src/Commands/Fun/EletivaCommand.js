835bfdaeade1800a9a8e74638d9a2e35fbc4b158
ADG <adg@adgdev.me>
2022-06-01 07:21:21 -0300

    Create EletivaCommand.js

---
+++ src/Commands/Fun/EletivaCommand.js
@@ -0,0 +1,46 @@
+module.exports = class NitroCommand {
+	constructor () {
+		return {
+			permissoes: {
+				membro: [],
+				bot: [],
+				dono: false
+			},
+			pt: {
+				nome: 'eletiva',
+				categoria: '⭐ » Diversão',
+				desc: 'Eletiva'
+			},
+			en: {
+				nome: 'eletiva,
+				categoria: '⭐ » Fun',
+				desc: 'Eletiva'
+			},
+			fr: {
+				nome: 'eletiva',
+				categoria: '⭐ » Divertissement',
+				desc: 'Eletiva'
+			},
+			/*
+			SUB_COMMAND	1 = SubCommand
+			SUB_COMMAND_GROUP: 2 = SubCommandGroup
+			STRING: 3 = String
+			INTEGER: 4 = Any integer between -2^53 and 2^53
+			BOOLEAN: 5 = True or False
+			USER: 6 = User Mention
+			CHANNEL: 7 = Includes all channel types + categories
+			ROLE: 8 = Role Mention
+			MENTIONABLE: 9 = Includes users and roles
+			NUMBER: 10 = Any double between -2^53 and 2^53
+			*/
+			options: [],
+			aliases: ['fakenitro', 'fake-nitro', 'nitrofake', 'gift', 'giftnitro', 'nitrogift'],
+			run: this.run
+		};
+	}
+
+	async run (ctx) {
+		const number = return Math.random() * (20 - 1) + 1;
+		ctx.message.channel.slashReply('🎲 **|** Toma aqui o número: **' + number + '**')
+	}
+};
