4c392cc91d3350b1a0a74987f6fedabe779ce5e4
ADG <adg@adgdev.me>
2022-06-01 07:46:15 -0300

    Update EletivaCommand.js

--- src/Commands/Fun/EletivaCommand.js
+++ src/Commands/Fun/EletivaCommand.js
@@ -40,7 +40,7 @@ module.exports = class NitroCommand {
 	}
 
 	async run (ctx) {
-		const number = Math.random() * (20 - 1) + 1;
+		const number = Math.floor(Math.random() * 20) + 1;
 		ctx.message.channel.slashReply('🎲 **|** Toma aqui o número: **' + Number(number).toFixed(0) + '**')
 	}
 };
