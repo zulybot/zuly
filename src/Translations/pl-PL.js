e176a735c818c65e881025fdd9fd541336e4932a
ADG <2007andredoe@gmail.com>
2022-07-17 11:07:28 -0300

    New translations en-US.js (Polish)

--- src/Translations/pl-PL.js
+++ src/Translations/pl-PL.js
@@ -51,7 +51,7 @@ module.exports = {
 		coletado: 'You have collected **%m** daily, come back tomorrow and collect again.',
 		coletou: 'You have already collected your daily reward, collect it again in **%time**'
 	},
-	premium: 'You have discovered a premium feature, to be a premium user join my [support server](<https://discord.gg/pyyyJpw5QW>)!',
+	premium: 'You have discovered a premium feature, to be a premium user join my [support server](<https://discord.gg/8SA5sfyR7g>)!',
 	ball: {
 		title: '🎱 Magiczna Kula',
 		field1: '**Pytanie:**',
@@ -163,7 +163,7 @@ module.exports = {
 	],
 	botban: {
 		title: 'Zbanowano Cię!',
-		description: '↳ You have been banned from using `%z` by: `%r`\n> <:zu_info:911303533859590144> You can file an appeal on our [support server](https://discord.gg/pyyyJpw5QW).'
+		description: '↳ You have been banned from using `%z` by: `%r`\n> <:zu_info:911303533859590144> You can file an appeal on our [support server](https://discord.gg/8SA5sfyR7g).'
 	},
 	customCommand: 'Polecenie zostało utworzone, użyj `/%n`, aby wykonać.',
 	alreadyExists: 'Polecenie już istnieje.',
@@ -267,7 +267,7 @@ module.exports = {
 		c: 'Zaczekaj %t sekund przed użyciem kolejnego polecenia.',
 		e: 'Ups, wystąpił błąd!',
 		e2: 'Zgłoś ten błąd!',
-		e3: 'My team probably already knows about this error, but how about you help? You can open an [issue](https://github.com/zulybot/zuly/issues) in [github](https://github.com/zulybot/zuly) or report it to my [support server](https://discord.gg/pyyyJpw5QW), let\'s make the discord a better place, together 🤝',
+		e3: 'My team probably already knows about this error, but how about you help? You can open an [issue](https://github.com/zulybot/zuly/issues) in [github](https://github.com/zulybot/zuly) or report it to my [support server](https://discord.gg/8SA5sfyR7g), let\'s make the discord a better place, together 🤝',
 		nsfw: 'Tego polecenia można użyć tylko na kanałach z treściami dla dorosłych'
 	},
 	host: {
@@ -297,7 +297,7 @@ module.exports = {
 	help: {
 		title: 'Lista poleceń • ',
 		creators: 'Stworzyli mnie ',
-		description: '> My current prefix is: `%p`\n> If you have any questions, please enter my support server: [Click here](https://discord.gg/pyyyJpw5QW)\n> Add me by clicking [here](https://discord.com/oauth2/authorize?client_id=880173509077266483&scope=bot&permissions=805432446)',
+		description: '> My current prefix is: `%p`\n> If you have any questions, please enter my support server: [Click here](https://discord.gg/8SA5sfyR7g)\n> Add me by clicking [here](https://discord.com/oauth2/authorize?client_id=880173509077266483&scope=bot&permissions=805432446)',
 		nsfw: 'You need to be on an NSFW channel to see nsfw commands. '
 	},
 	calc: {
@@ -357,7 +357,7 @@ module.exports = {
 	},
 	invite: {
 		add: 'Dodaj mnie do swojego serwera!',
-		desc: 'Yay, did you like my commands? Or my features? [Add me](https://discord.com/oauth2/authorize?client_id=719524114536333342&scope=bot%20applications.commands&permissions=805432446)\n\nYou need Help or something? Log into my [support server](https://discord.gg/pyyyJpw5QW), yay'
+		desc: 'Yay, did you like my commands? Or my features? [Add me](https://discord.com/oauth2/authorize?client_id=719524114536333342&scope=bot%20applications.commands&permissions=805432446)\n\nYou need Help or something? Log into my [support server](https://discord.gg/8SA5sfyR7g), yay'
 	},
 	ban: {
 		noarg: '**Mention** some user or give the **ID** of the same.',
